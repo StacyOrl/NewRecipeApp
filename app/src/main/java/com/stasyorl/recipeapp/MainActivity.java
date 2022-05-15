240cd011568d9ec2732b2ab22858c86c85640e29
Anastasia <orliknastushik@gmail.com>
2022-05-15 23:25:10 +0200

    fragments

--- app/src/main/java/com/stasyorl/recipeapp/MainActivity.java
+++ app/src/main/java/com/stasyorl/recipeapp/MainActivity.java
@@ -1,22 +1,26 @@
 package com.stasyorl.recipeapp;
 
-import android.app.ActivityOptions;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.stasyorl.recipeapp.Adapters.CategoryAdapter;
 import com.stasyorl.recipeapp.Adapters.RandomRecipeAdapter;
+import com.stasyorl.recipeapp.Fragments.UserRegistrationFragment;
 import com.stasyorl.recipeapp.Listeners.CategoryListener;
 import com.stasyorl.recipeapp.Listeners.RandomRecipeResponseListener;
 import com.stasyorl.recipeapp.Listeners.RecipeClickListener;
@@ -37,8 +41,21 @@ public class MainActivity extends AppCompatActivity implements CategoryListener 
     ProgressDialog dialog;
     RequestManager manager;
     RandomRecipeAdapter randomRecipeAdapter;
+//    RandomRecipeResponseListener randomRecipeResponseListener;
     CategoryAdapter categoryAdapter;
     RecyclerView recyclerView, recycle_category;
+    ImageView no_wifi_image, imageView_user_pic;
+    FrameLayout fragmentContainer;
+    LinearLayout mainScreen;
+
+    InternetConnectorReceiver receiver;
+
+    Fragment fragment, categoryFragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+
+
 
 
 
@@ -48,8 +65,36 @@ public class MainActivity extends AppCompatActivity implements CategoryListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+//        receiver = new InternetConnectorReceiver(MainActivity.this);
+//        registerReceiver(receiver,new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+//        no_wifi_image = findViewById(R.id.no_wifi_image);
+
+        fragment = new UserRegistrationFragment();
+        fragmentContainer = findViewById(R.id.fragmentContainer);
+        mainScreen = findViewById(R.id.main_screen);
+        imageView_user_pic = findViewById(R.id.imageView_user_pic);
+        imageView_user_pic.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "KLAPPT", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+                mainScreen.setVisibility(View.GONE);
+                fragmentContainer.setVisibility(View.VISIBLE);
+
+
+
+        });
+
+
+
+
+
         dialog = new ProgressDialog(this);
         dialog.setTitle("Loading...");
+
+        imageView_user_pic = findViewById(R.id.imageView_user_pic);
+
+
 
         searchView = findViewById(R.id.searchView_home);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -84,6 +129,30 @@ public class MainActivity extends AppCompatActivity implements CategoryListener 
 
 
     }
+//
+//    public void addFragment(){
+//        fragment = new UserRegistrationFragment();
+//        fragmentManager = getFragmentManager();
+//        fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.fragmentContainer, fragment);
+//        fragmentTransaction.commit();
+//
+//    }
+    public void changeUI(boolean isConnected) {
+        // Change status according to boolean value
+        MainActivity.this.runOnUiThread(() -> {
+            if (isConnected) {
+                Toast.makeText(MainActivity.this, "yes internet", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+
+            } else {
+                Toast.makeText(MainActivity.this, "no internet", Toast.LENGTH_SHORT).show();
+                no_wifi_image.setVisibility(View.VISIBLE);
+                dialog.dismiss();
+            }
+        });
+    }
+
 
     private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
         @Override
@@ -99,6 +168,8 @@ public class MainActivity extends AppCompatActivity implements CategoryListener 
         @Override
         public void didError(String message) {
             Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+            no_wifi_image = findViewById(R.id.no_wifi_image);
 
         }
     };
@@ -136,6 +207,4 @@ public class MainActivity extends AppCompatActivity implements CategoryListener 
         manager.getRandomRecipes(randomRecipeResponseListener, tags);
         dialog.show();
     }
-
-
 }
